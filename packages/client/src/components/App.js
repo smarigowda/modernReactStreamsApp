eeb93055b2eec470eddc31e54e9fc40a2d07ae7e
Santosh Marigowda <santosharakere@gmail.com>
2019-05-05 15:42:07 +0100

    sample router

--- packages/client/src/components/App.js
+++ packages/client/src/components/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+const PageOne = () => {
+    return (
+        <h1>Page 1</h1>
+    )
+}
+const PageTwo = () => {
+    return (
+        <h1>Page 2</h1>
+    )
+}
+const Home = () => {
+    return (
+        <h1>Home Page</h1>
+    )
+}
 export default () => {
     return (
-        <h1>A React Streaming App !</h1>
+        <div>
+            <BrowserRouter>
+                <Route path="/" exact component={Home} />
+                <Route path="/page1" component={PageOne} />
+                <Route path="/page2" component={PageTwo} />
+            </BrowserRouter>
+        </div>
     );
 };
