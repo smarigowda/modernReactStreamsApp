2f881a5fc9e065fdfef486490e8b148b106291f3
Santosh Marigowda <santosharakere@gmail.com>
2019-05-05 22:00:53 +0100

    loading client libraties using gapi.load

--- packages/client/src/components/GoogleAuth.js
+++ packages/client/src/components/GoogleAuth.js
@@ -3,7 +3,17 @@ import React, { useEffect } from 'react';
 export const GoogleAuth = () => {
     useEffect(() => {
         console.log(window.gapi);
-        window.gapi.load('client:auth2');
+        async function loadLibraries () {
+            console.log('waiting for 10 sec...');
+            await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve();
+                }, 10000);
+            });
+            console.log('loading libraries...');
+            await window.gapi.load('client:auth2');
+        }
+        loadLibraries();
     }, [])
     return (
         <h1>Google Auth</h1>
