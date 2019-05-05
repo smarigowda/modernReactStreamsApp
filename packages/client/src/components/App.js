3ca9963117cc6b48f147adc98d1dcf7af8f9035f
Santosh Marigowda <santosharakere@gmail.com>
2019-05-05 15:58:29 +0100

    navigation using anchor tag

--- packages/client/src/components/App.js
+++ packages/client/src/components/App.js
@@ -3,24 +3,39 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 const PageOne = () => {
     return (
-        <h1>Page 1</h1>
+        <>
+            <h1>Page 1</h1>
+            <a href="/page2">Navigate to Page 2</a>
+            <br/>
+            <a href="/">Navigate to Home</a>
+        </>
     )
 }
 const PageTwo = () => {
     return (
-        <h1>Page 2</h1>
+        <div>
+            <h1>Page 2</h1>
+            <a href="/page1">Navigate to Page 1</a>
+            <br/>
+            <a href="/">Navigate to Home</a>
+        </div>
     )
 }
 const Home = () => {
     return (
-        <h1>Home Page</h1>
+        <>
+            <h1>Home Page</h1>
+            <a href="/page1">Navigate to Page 1</a>
+            <br/>
+            <a href="/page2">Navigate to Page 2</a>
+        </>
     )
 }
 export default () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path="/" exact component={Home} />
+                <Route path="/" exact={true} component={Home} />
                 <Route path="/page1" component={PageOne} />
                 <Route path="/page2" component={PageTwo} />
             </BrowserRouter>
