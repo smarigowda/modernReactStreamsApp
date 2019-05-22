f67d24e7c0034cb154540095e45894510c33810d
Santosh Marigowda <santosharakere@gmail.com>
2019-05-22 21:51:28 +0100

    removed justify self so that the grid element occupies full width

--- packages/client/src/styled-components/styledLink.js
+++ packages/client/src/styled-components/styledLink.js
@@ -3,13 +3,11 @@ import styled from 'styled-components';
 
 export default styled(Link)`
         padding: 1rem;
-        justify-self: center;
         align-self: center;
         text-decoration: none;
         font-size: 1.6rem;
         text-transform: uppercase;
         color: black;
-        width: 80%;
         text-align: center;
         &:hover {
             background-color: palevioletred;
