1b3443feb014402538604df7c788d8b426aaf289
Santosh Marigowda <santosharakere@gmail.com>
2019-05-25 06:20:45 +0100

    refactor

--- packages/client/src/components/GoogleAuth.js
+++ packages/client/src/components/GoogleAuth.js
@@ -1,55 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from '../styled/button';
+import React, { useState, useEffect } from "react";
+import { Button } from "../styled/button";
 
 const { REACT_APP_CLIENT_ID } = process.env;
 
 export const GoogleAuth = () => {
-    const [isSignedIn, setIsSignedIn] = useState(null);
-    const [auth, setAuth] = useState({});
-    console.log(`current state of isSignedIn = ${isSignedIn} [GoogleAuth]`)
-    useEffect(() => {
-        function loadLibraries() {
-            console.log('loadLibraries called... [useEffect]');
-            window.gapi.load('client:auth2', async () => {
-                console.log('initiating gapi client... [useEffect]');
-                window.gapi.client.init({
-                    clientId: REACT_APP_CLIENT_ID,
-                    scope: 'email'
-                });
-                console.log('initiated... [useEffect]');
-                const auth = window.gapi.auth2.getAuthInstance();
-                console.log('setting auth... [useEffect]');
-                setAuth(auth); // renders the component immediately after this call
-                auth.isSignedIn.listen(() => {
-                    console.log(`setting isSignedIn to ${auth.isSignedIn.get()} [listen inside useEffect]`)
-                    setIsSignedIn(auth.isSignedIn.get()); // renders the component immediately after this call
-                });
-            });
-        }
-        loadLibraries();
-    }, []);
-    const onClickHandler = async () => {
-        console.log('button clicked....');
-        if (auth.isSignedIn.get()) {
-            await auth.signOut();
-        } else {
-            await auth.signIn();
-        }
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  const [auth, setAuth] = useState({});
+  useEffect(() => {
+    function loadLibraries() {
+      console.log("loadLibraries called... [useEffect]");
+      window.gapi.load("client:auth2", () => {
+        console.log("initiating gapi client... [useEffect]");
+        window.gapi.client.init({
+          clientId: REACT_APP_CLIENT_ID,
+          scope: "email"
+        });
+        console.log("gapi client initiated... [useEffect]");
+        const auth = window.gapi.auth2.getAuthInstance();
+        console.log("setting auth... [useEffect]");
+        setAuth(auth); // renders the component immediately after this call
+        console.log(
+          `setting isSignedIn to ${auth.isSignedIn.get()} [inside useEffect]`
+        );
+        onAuthChange(auth);
+        auth.isSignedIn.listen(() => {
+          console.log(
+            `setting isSignedIn to ${auth.isSignedIn.get()} [inside listen of useEffect]`
+          );
+          onAuthChange(auth);
+        });
+      });
     }
-    const renderLoginButton = () => {
-        console.log('rendering login Button [renderLoginButton]');
-        let button;
-        if(isSignedIn === null) {
-            button = null;
-            return button; // do not render anything if signed in state is unknown (null)
-        } 
-        button = (
-            <Button
-                onClick={onClickHandler}>
-                {isSignedIn ? 'Sign Out' : 'Sign In With Google'}
-            </Button>
-        )
-        return button;
+    loadLibraries();
+  }, []);
+  const onAuthChange = auth => {
+    setIsSignedIn(auth.isSignedIn.get()); // renders the component immediately after this call
+  };
+  const onClickHandler = async () => {
+    console.log("button clicked....");
+    if (auth.isSignedIn.get()) {
+      await auth.signOut();
+    } else {
+      await auth.signIn();
     }
-    return renderLoginButton();
+  };
+  const renderLoginButton = () => {
+    console.log("rendering login Button [renderLoginButton]");
+    let button;
+    let text;
+    if (isSignedIn === true) {
+      text = "Sign Out";
+    } else if (isSignedIn === false) {
+      text = "Sign In With Google";
+    } else if (isSignedIn === null) {
+      text = "...";
+    }
+    button = <Button onClick={onClickHandler}>{text}</Button>;
+    return button;
+  };
+  return renderLoginButton();
 };
