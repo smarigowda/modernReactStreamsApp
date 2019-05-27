fb1c75a931ea2b7f7e7202e0c590e9a6b38ff5db
Santosh Marigowda <santosharakere@gmail.com>
2019-05-27 17:52:30 +0100

    fixed border style issue

--- packages/client/src/styled/input.js
+++ packages/client/src/styled/input.js
@@ -4,10 +4,11 @@ export const Input = styled.input`
   font-size: 1rem;
   width: 250px;
   padding: 1rem;
+  border: 1px solid black;
   &:focus {
     outline: none;
     box-shadow: 0 0 1px rgba(81, 203, 238, 1);
-    border: .2px solid rgba(81, 203, 238, 1);
+    border: 1px solid rgba(81, 203, 238, 1);
     transition: all 0.30s ease-out;
   }
 `;
