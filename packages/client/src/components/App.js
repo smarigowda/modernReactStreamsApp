7a344a28e248d3c28f2eb2702947dab6879e25b3
Santosh Marigowda <santosharakere@gmail.com>
2019-06-02 14:11:33 +0100

    plain router with custom history

--- packages/client/src/components/App.js
+++ packages/client/src/components/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import { Router, Route } from "react-router-dom";
 
-import { Header } from './Header';
-import StreamCreate from './streams/StreamCreate';
-import StreamDelete from './streams/StreamDelete';
-import StreamList from './streams/StreamList';
-import StreamEdit from './streams/StreamEdit';
-import StreamShow from './streams/StreamShow';
+import { Header } from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+
+import history from "../history";
 
 export default () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Header />
-                <Route path="/" exact={true} component={StreamList} />
-                <Route path="/new" exact={true} component={StreamCreate} />
-                <Route path="/Edit" exact={true} component={StreamEdit} />
-                <Route path="/delete" exact={true} component={StreamDelete} />
-                <Route path="/show" exact={true} component={StreamShow} />
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <Router history={history}>
+        <Header />
+        <Route path="/" exact={true} component={StreamList} />
+        <Route path="/new" exact={true} component={StreamCreate} />
+        <Route path="/Edit" exact={true} component={StreamEdit} />
+        <Route path="/delete" exact={true} component={StreamDelete} />
+        <Route path="/show" exact={true} component={StreamShow} />
+      </Router>
+    </div>
+  );
 };
