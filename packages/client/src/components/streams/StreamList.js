4c0b60df5c8ef7574761ba1c15b0cf2865ec2715
Santosh Marigowda <santosharakere@gmail.com>
2019-06-02 17:02:26 +0100

    create stream button at Home Page

--- packages/client/src/components/streams/StreamList.js
+++ packages/client/src/components/streams/StreamList.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+
+import { Button } from "../../styled/button";
+import history from "../../history";
+
+const onClickHandler = () => {
+  history.push("/new");
+};
 
 export default () => {
-    return (
-        <div>
-            <h1>Stream List</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Button onClick={onClickHandler}>Create Stream</Button>
+    </div>
+  );
 };
--- packages/client/src/styled/button.js
+++ packages/client/src/styled/button.js
@@ -4,8 +4,12 @@ export const Button = styled.button`
     font-size: 1.5rem;
     background-color: white;
     color: black;
-    border: none;
+    border: black solid 1px;
+    padding: 15px;
+    padding-left: 30px;
+    padding-right: 30px;
     cursor: pointer;
+    border-radius: 3px;
     &:focus {
         outline: none;
     }
