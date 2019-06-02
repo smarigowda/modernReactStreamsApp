9a4b11b2a8284bac49451f87f295e4ed1905a7b3
Santosh Marigowda <santosharakere@gmail.com>
2019-06-02 12:52:38 +0100

    create stream reducer implemented

---
+++ packages/client/src/reducers/streamsReducer.js
@@ -0,0 +1,10 @@
+import { CREATE_STREAM } from '../actions/types';
+
+export default (state = {}, action) => {
+    switch(action.type) {
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload }
+        default: 
+            return state;
+    }
+}
